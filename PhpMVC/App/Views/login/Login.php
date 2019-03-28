324c53b1d1050556a082fe5cdcea40a33fbac4a4
Zerphyrum <36630135+Zerphyrum@users.noreply.github.com>
2019-03-28 00:22:42 -0300

    Tela de login nova
    Novo layout para página de login.
    by Gélida

--- PhpMVC/App/Views/login/Login.php
+++ PhpMVC/App/Views/login/Login.php
@@ -1,42 +1,84 @@
-<html>
-<head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link href="<?php echo HOME;?>/public/css/main.css" rel="stylesheet" type="text/css">
+<!DOCTYPE html>
+<html lang="en">
 
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width">
-	<title>Login</title>
-</head>
+    <head>
 
-<body >
-	<div class="container vertical-center col-7 " >
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Login</title>
 
-		<div class="card centerx1 border-primary ">
+        <!-- CSS -->
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <link rel="stylesheet" href="<?php echo HOME;?>/public/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo HOME;?>/public/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo HOME;?>/public/css/form-elements.css">
+        <link rel="stylesheet" href="<?php echo HOME;?>/public/css/style.css">
 
-			<img src="<?php echo HOME; ?>\public\Icons\imgcad.png" class="card-top" style="max-height: 300px; max-width: 300px;">
-			<form>
-				<div class="form-group">
-					<input type="Usuário" class="form-control " id="" placeholder="Usuário" required="">
-				</div>
-				<div class="form-group">
-					<input type="password" id="inputPassword" class="form-control" placeholder="validacao()" required="">
-				</div>
-				<div class="form-group">
-					<div class="col-6">
-						<button type="btn" class="btn btn-primary">Entrar</button>
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-6" style="padding-left: 10px">
-					<a <?php if($viewVar['nameController'] == "CadastroController") { ?> class="active" <?php  } ?> target="_blank" href="http://<?php echo APP_HOST; ?>/Cadastro/index" >Criar conta</a>
+        <!-- Paradinha identificar -->
+        <link rel="shortcut icon" href="<?php echo HOME;?>/public/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo HOME;?>/public/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo HOME;?>/public/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="<?php echo HOME;?>/public/ico/apple-touch-icon-57-precomposed.png">
+    </head>
+    <body>
+        <div class="top-content">
+        	
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8 col-sm-offset-2 text">
+                            
+                            <div class="description">
+                            	<p>
 
-						
-					</div>
-				</div>
-			</form>	
-		</div>
-	</div>
-</body>
+                            	</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                        	<div class="form-top">
+ 								<img src="<?php echo HOME;?>/public/img/backgrounds/8.png" class="form-top" align="center"style="padding-left: 50px">
+              
+                            
+                            </div>
+                           
+                            <div class="form-bottom">
+			                    <form role="form" action="" method="post" class="login-form">
+			                    	<div class="form-group">
+			                    		<label class="sr-only" for="form-username">Usuário</label>
+			                        	<input type="text" name="form-username" placeholder="Usuário..." class="form-username form-control" id="form-username">
+			                        </div>
+			                        <div class="form-group">
+			                        	<label class="sr-only" for="form-password">Senha</label>
+			                        	<input type="password" name="form-password" placeholder="Senha..." class="form-password form-control" id="form-password">
+			                        </div>
+                                    <button type="submit" class="btn">ENTRAR</button>
+                                
+                                    <div class="col-6" style="padding-left: 195px">
+                                        <input class="btn btn-light" name="Criar conta" type="button" onClick="window.open('http://localhost/Npalunos2/cadastro-view/telacadastroprof.php')" value="Criar conta">
+
+			                    </form>
+		                    </div>
+                        </div>
+                    </div>
+                    <div class="row">              
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+        <!-- Javascript -->
+        <script src="<?php echo HOME;?>/public/js/jquery-1.11.1.min.js"></script>
+        <script src="<?php echo HOME;?>/public/bootstrap/js/bootstrap.min.js"></script>
+        <script src="<?php echo HOME;?>/public/js/jquery.backstretch.min.js"></script>
+        <script src="<?php echo HOME;?>/public/js/scripts.js"></script>
+
+
+    </body>
+
 </html>
-
-
